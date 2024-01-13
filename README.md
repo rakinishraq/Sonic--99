7271f9a2c41605a204ffb6a17871acdc624e35b3
Rakin Ishraq <holla@thatkidfrom.nyc>
2024-01-12 23:10:28 -0500

    starter vr support

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Vaporwave Sonic future retro (1999 x 2099) low-poly fighter jets racing in space
 
 DIGITALDREAM font
+Assets: InstanceDock, anthonyec.camera_preview, godot-xr-tools
--- player/player.gd
+++ player/player.gd
@@ -41,3 +41,19 @@ func _physics_process(delta):
 	var rounded = round(speed * 10) / 10.0
 	$CanvasLayer/RichTextLabel1.set_text("[center]"+str(rounded))
 	$CanvasLayer/TextureRect.rotation_degrees = rad_to_deg(rotation.z)
+
+
+var xr_interface: XRInterface
+	
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		# Change our main viewport to output to the HMD
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
